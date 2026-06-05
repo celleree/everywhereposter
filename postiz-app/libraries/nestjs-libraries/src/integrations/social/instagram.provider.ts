83b7fd598955dff2a83c107a8f1d01c9d95031eb
Arundel <arundel@example.com>
2026-06-05 04:20:39 +0000

    Revert "Stop faking Instagram total metric charts"
    This reverts commit 668ba6ed310da88b42a8bf29e3ad79e3e15bf534.

--- postiz-app/libraries/nestjs-libraries/src/integrations/social/instagram.provider.ts
+++ postiz-app/libraries/nestjs-libraries/src/integrations/social/instagram.provider.ts
@@ -2423,6 +2423,10 @@ export class InstagramProvider
             total: d.total_value.value,
             date: dayjs().format('YYYY-MM-DD'),
           },
+          {
+            total: d.total_value.value,
+            date: dayjs().add(1, 'day').format('YYYY-MM-DD'),
+          },
         ],
       }))
     );
