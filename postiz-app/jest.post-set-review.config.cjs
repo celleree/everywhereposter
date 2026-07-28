2cf03448a22576ee757d4b4814310a016ce95c61
celleree <113010938+celleree@users.noreply.github.com>
2026-07-28 11:21:51 -0700

    Add generated post-set review Jest config

---
+++ postiz-app/jest.post-set-review.config.cjs
@@ -0,0 +1,29 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.base.json');
+
+module.exports = {
+  rootDir: __dirname,
+  testEnvironment: 'jsdom',
+  clearMocks: true,
+  testTimeout: 20000,
+  setupFiles: ['reflect-metadata'],
+  testMatch: ['<rootDir>/tests/frontend/**/*.spec.tsx'],
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          ...compilerOptions,
+          module: 'commonjs',
+          jsx: 'react-jsx',
+          isolatedModules: true,
+        },
+        diagnostics: false,
+      },
+    ],
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  modulePathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/apps/.*/dist'],
+};
