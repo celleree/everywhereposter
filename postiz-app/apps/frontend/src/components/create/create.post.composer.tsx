1cf4e31c4f6445f421ea846c08d58f8d807ee468
celleree <113010938+celleree@users.noreply.github.com>
2026-08-03 13:42:21 -0700

    Use type-only composer props import

--- postiz-app/apps/frontend/src/components/create/create.post.composer.tsx
+++ postiz-app/apps/frontend/src/components/create/create.post.composer.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import React, { FC } from 'react';
-import {
-  AddEditModal,
-  AddEditModalProps,
-} from '@gitroom/frontend/components/new-launch/add.edit.modal';
+import { AddEditModal } from '@gitroom/frontend/components/new-launch/add.edit.modal';
+import type { AddEditModalProps } from '@gitroom/frontend/components/new-launch/add.edit.modal';
 
 export const isGuidedComposerShellEnabled = (
   value = process.env.NEXT_PUBLIC_GUIDED_COMPOSER_SHELL
