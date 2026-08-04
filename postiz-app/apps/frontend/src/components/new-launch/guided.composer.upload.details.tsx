5b362d58067560c1835f526b0abbc44aa76d4b2f
celleree <113010938+celleree@users.noreply.github.com>
2026-08-04 10:48:16 -0700

    Hide legacy guided upload actions by position

--- postiz-app/apps/frontend/src/components/new-launch/guided.composer.upload.details.tsx
+++ postiz-app/apps/frontend/src/components/new-launch/guided.composer.upload.details.tsx
@@ -292,7 +292,7 @@ export const GuidedComposerUploadDetails: FC<{
             display: none !important;
           }
 
-          ${GUIDED_UPLOAD_SECTION_SELECTOR}.${GUIDED_UPLOAD_PROGRESS_CLASS} > div:nth-child(2) > div:first-of-type button[disabled] {
+          ${GUIDED_UPLOAD_SECTION_SELECTOR}.${GUIDED_UPLOAD_PROGRESS_CLASS} > div:nth-child(2) > div:first-of-type > div:first-child > button:nth-child(-n + 2) {
             display: none !important;
           }
         `}
